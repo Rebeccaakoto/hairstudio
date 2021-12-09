0a22fe42eba6a4d6c20ae6376e6deab6f928ec7c
rebeccaakoto <rebeccaakoto@github.com>
2021-12-09 10:23:22 +0000

    make it better

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -1,9 +1,9 @@
 <?php
 
 
-define('SERVER', 'us-cdbr-east-04.cleardb.com');
-define('USER', 'b3982c992df8db');
-define('DATABASE', 'hair');
-define('PASSWORD', 'b2fbbde0');
+    define('SERVER', 'us-cdbr-east-04.cleardb.com');
+    define('USER', 'b3982c992df8db');
+    define('DATABASE', 'heroku_010769669e7dc98');
+    define('PASSWORD', 'b2fbbde0');
 
 ?>
